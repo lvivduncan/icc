56896b10d8fd8caa78c3ce7a0627dba3dd6f8b69
lvivduncan <lvivduncan@gmail.com>
2021-08-27 00:25:27 +0300

    footer

--- js/script.js
+++ js/script.js
@@ -8,41 +8,119 @@ $('#switch').on('click', function(){
 
 // slide 3
 $(document).ready(function() {
-    if ( $(window).width() < 996 ) {
-        startCarousel();
+    if ( $(window).width() > 996 ) {
+        stopCarousel();
+        
+        $('#slide-3-carousel').addClass('off');
+        $('#slide-7-carousel').addClass('off');
+        $('#slide-9-carousel').addClass('off');
     } else {
-        $('.owl-carousel').addClass('off');
+        startCarousel();
+
+        $('#slide-3-carousel').removeClass('off');
+        $('#slide-7-carousel').removeClass('off');
+        $('#slide-9-carousel').removeClass('off');
     }
 });
 
 $(window).resize(function() {
-    if ( $(window).width() < 996 ) {
-        startCarousel();
-    } else {
+    if ( $(window).width() > 996 ) {
         stopCarousel();
+        
+        $('#slide-3-carousel').addClass('off');
+        $('#slide-7-carousel').addClass('off');
+        $('#slide-9-carousel').addClass('off');
+    } else {
+        startCarousel();
+
+        $('#slide-3-carousel').removeClass('off');
+        $('#slide-7-carousel').removeClass('off');
+        $('#slide-9-carousel').removeClass('off');
     }
 });
   
 function startCarousel(){
-    $("#slide-3-carousel").owlCarousel({
-        navigation : true, // Show next and prev buttons
-        slideSpeed : 500,
-        margin:10,
-        paginationSpeed : 400,
-        autoplay:true,
-        items : 1,
-        itemsDesktop : false,
-        itemsDesktopSmall : false,
-        itemsTablet: false,
-        itemsMobile : false,
+    $('#slide-3-carousel', '#slide-7-carousel').owlCarousel({
         loop:true,
-        nav:true,
-        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right' aria-hidden='true'></i>"]
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            },
+            776:{
+                items:3,
+                nav:true
+            },
+            996:{
+                items:4,
+                nav:true,
+                loop:false
+            }
+        }
+    });
+
+    $('#slide-9-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:true
+            }
+        }
     });
 }
 
 function stopCarousel() {
-    var owl = $('.owl-carousel');
-    owl.trigger('destroy.owl.carousel');
-    owl.addClass('off');
+    var owl1 = $('#slide-3-carousel');
+    owl1.trigger('destroy.owl.carousel');
+    owl1.addClass('off');
+
+    var owl2 = $('#slide-7-carousel');
+    owl2.trigger('destroy.owl.carousel');
+    owl2.addClass('off');
+
+    var owl3 = $('#slide-9-carousel');
+    owl3.trigger('destroy.owl.carousel');
+    owl3.addClass('off');    
 }
+
+$('#client .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:true
+        },
+        776:{
+            items:3,
+            nav:true
+        },
+        996:{
+            items:4,
+            nav:true
+        },
+        1200:{
+            items:5,
+            nav:true
+        }
+    }
+});
